26fd47f17a1432ae89a193f4c9526dbed968b788
holiczab <holiczabarnabas@gmail.com>
2022-10-14 15:01:09 +0200

    GUI part2

---
+++ SkinApp/app/src/main/java/com/example/skinapp/AllDiagnosesActivity.java
@@ -0,0 +1,14 @@
+package com.example.skinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllDiagnosesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_diagnoses);
+    }
+}
---
+++ SkinApp/app/src/main/java/com/example/skinapp/OfflineActivity.java
@@ -0,0 +1,14 @@
+package com.example.skinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OfflineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offline);
+    }
+}
