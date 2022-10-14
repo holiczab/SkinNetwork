a08b1bd5fbda7e3438b5ee3afbb4d305e4a3a1bc
holiczab <holiczabarnabas@gmail.com>
2022-10-14 15:58:32 +0200

    GUI part2

--- SkinApp/app/src/main/java/com/example/skinapp/MainActivity.java
+++ SkinApp/app/src/main/java/com/example/skinapp/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.skinapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.skydoves.powermenu.MenuAnimation;
+import com.skydoves.powermenu.PowerMenu;
+import com.skydoves.powermenu.PowerMenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    final Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
