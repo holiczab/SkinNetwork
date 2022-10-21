c169d69dc3652d76c1d4c2dc75d3e8a847076420
Dávid Magyar <aoloyk@INF.ELTE.HU>
2022-10-21 17:26:06 +0200

    Add controller.py

---
+++ src/desktop_app/controller.py
@@ -0,0 +1,56 @@
+from main_window import MainWindow
+
+
+class Controller(object):
+    """
+    Class for controlling the application.
+
+    """
+
+    def __init__(self, view: MainWindow) -> None:
+        """
+        Constructor of the Controller class.
+
+        :param view: an instance of MainWindow
+        :return: None
+        """
+
+        super(Controller, self).__init__()
+
+        self.view = view
+
+        self.__bind_commands()
+
+    def mainloop(self) -> None:
+        """
+        The main loop of the application.
+
+        :return: None
+        """
+
+        self.view.show()
+
+    def __bind_commands(self) -> None:
+        """
+        Binds all commands to the components on the main window.
+
+        :return: None
+        """
+
+        self.view.upload_btn.configure(command=self.__upload_button_clicked)
+
+    def __upload_button_clicked(self) -> None:
+        """
+        Handles the upload button clicked event.
+
+        :return: None
+        """
+
+        path = self.view.open_files()
+        self.view.show_image(path)
+
+    def __more_info_button_clicked(self):
+        pass
+
+    def __generate_report_button_clicked(self):
+        pass
